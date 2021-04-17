b7330579d1506579629a6cf6f3da17655811c317
Jinyoung Kim <jingood2@Jinyoungui-MacBookAir.local>
2021-04-17 16:00:33 +0900

    minor update

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,10 +2,10 @@ import * as chalk from 'chalk';
 
 export const envVars = {
   REGION: process.env.REGION || 'ap-northeast-2',
-  PROJECT_NAME: 'CDK_PIPELINES_TEMPLATE',
+  PROJECT_NAME: 'magicmall',
   APP_NAME: process.env.APP_NAME || 'myapp',
   REPO_OWNER: process.env.REPO_OWNER || 'jingood2',
-  REPO_NAME: process.env.REPO_NAME || 'magicmall-eb-codepipeline',
+  REPO_NAME: process.env.REPO_NAME || 'magicmall-eb-codepipeline-stack',
   BUILD_BRANCH: process.env.BUILD_BRANCH || 'main',
   DEV_STAGE_ENV: {
     VPC_ID: 'vpc-07db512afbc65d743',
